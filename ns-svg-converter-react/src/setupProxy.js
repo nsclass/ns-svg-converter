98c4d3e879b3782a22552e68c399c0a59c74c06a
Nam Seob Seo <nsclass@gmail.com>
2021-09-06 11:34:14 -0500

    upgrade react app

--- ns-svg-converter-react/src/setupProxy.js
+++ ns-svg-converter-react/src/setupProxy.js
@@ -1,8 +1,8 @@
-const proxy = require("http-proxy-middleware")
+const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function (app) {
     app.use(
         "/api",
-        proxy({
+        createProxyMiddleware({
             target: "http://localhost:8080",
             changeOrigin: true,
         })
