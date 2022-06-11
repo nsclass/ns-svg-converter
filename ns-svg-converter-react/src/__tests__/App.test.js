2405a6aab73340e5beaaab5a0d15b363100e3441
Nam Seob Seo <nsclass@gmail.com>
2022-06-11 10:15:07 -0500

    check mainNav

--- ns-svg-converter-react/src/__tests__/App.test.js
+++ ns-svg-converter-react/src/__tests__/App.test.js
@@ -22,7 +22,7 @@ import React from "react"
 import {render} from "@testing-library/react"
 import {App} from "../App"
 
-test("renders learn react link", () => {
+test("main nav should exist", () => {
     const app = render(<App/>)
     const nav = app.findByTestId("mainNav")
     expect(nav).toBeDefined()
