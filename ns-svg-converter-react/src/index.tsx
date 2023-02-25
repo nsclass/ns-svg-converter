b336d4a487926678f151bfcaa7e5f5a17c39b05d
Nam Seob Seo <nsclass@gmail.com>
2023-02-25 15:44:15 -0600

    convert js to typescript

--- ns-svg-converter-react/src/index.tsx
+++ ns-svg-converter-react/src/index.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import {App} from "./App"
 
 const container = document.getElementById('root');
-// @ts-ignore
-const root = createRoot(container); // createRoot(container!) if you use TypeScript
-root.render(<App />);
+if (container) {
+    const root = createRoot(container); // createRoot(container!) if you use TypeScript
+    root.render(<App />);
+}
